fa42aae65504d9c2426d8d5f7ada47aa290e25cb
Fahd Mohammed <fahdmoh.1@gmail.com>
2024-02-13 23:08:54 +0000

    completed a simple go application

--- partyinvites/main.go
+++ partyinvites/main.go
@@ -5,12 +5,15 @@ import (
 	"html/template"
 	"net/http"
 )
+
 type Rsvp struct {
     Name, Email, Phone string
     WillAttend bool
 }
+
 var responses = make([]*Rsvp, 0, 10)
 var templates = make(map[string]*template.Template, 3)
+
 func loadTemplates() {
     templateNames := [5]string { "welcome", "form", "thanks", "sorry", "list" }
     for index, name := range templateNames {
@@ -19,19 +22,23 @@ func loadTemplates() {
             templates[name] = t
             fmt.Println("Loaded template", index, name)
         } else {
-panic(err) }
-} }
+            panic(err)
+        }
+    }
+}
+
 func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
     templates["welcome"].Execute(writer, nil)
 }
+
 func listHandler(writer http.ResponseWriter, request *http.Request) {
     templates["list"].Execute(writer, responses)
 }
+
 type formData struct {
     *Rsvp
     Errors []string
 }
-
 
 func formHandler(writer http.ResponseWriter, request *http.Request) {
     if request.Method == http.MethodGet {
@@ -45,7 +52,8 @@ func formHandler(writer http.ResponseWriter, request *http.Request) {
             Email: request.Form["email"][0],
             Phone: request.Form["phone"][0],
             WillAttend: request.Form["willattend"][0] == "true",
-}
+        }
+
         errors := []string {}
         if responseData.Name == "" {
             errors = append(errors, "Please enter your name")
@@ -59,28 +67,29 @@ func formHandler(writer http.ResponseWriter, request *http.Request) {
         if len(errors) > 0 {
             templates["form"].Execute(writer, formData {
                 Rsvp: &responseData, Errors: errors,
-            })
-		} else {
+            })            
+        } else {
             responses = append(responses, &responseData)
             if responseData.WillAttend {
                 templates["thanks"].Execute(writer, responseData.Name)
             } else {
                 templates["sorry"].Execute(writer, responseData.Name)
             }
-		}
-	} 
+        }
+    }
 }
 
-func main(){
-	loadTemplates()
 
-	http.HandleFunc("/", welcomeHandler)
-	http.HandleFunc("/list", listHandler)
-	http.HandleFunc("/form", formHandler)
 
-	err := http.ListenAndServe(":5001", nil)
-	if (err != nil ){
-		fmt.Println(err)
-	}
+func main() {
+    loadTemplates()
+
+    http.HandleFunc("/", welcomeHandler)
+    http.HandleFunc("/list", listHandler)
+    http.HandleFunc("/form", formHandler)
+
+    err := http.ListenAndServe(":5001", nil)
+    if (err != nil) {
+        fmt.Println(err)
+    }
 }
-
