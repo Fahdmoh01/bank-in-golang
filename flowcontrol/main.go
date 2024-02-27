22e887e29b47eb911f18e9a399da8ffc22c725a9
Fahd Mohammed <fahdmoh.1@gmail.com>
2024-02-27 22:01:07 +0000

    completed flow control chapter

---
+++ flowcontrol/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	//"strconv"
+)
+
+func main(){
+	// counter := 0
+	// product := "Kayak"
+	counter := 0 
+	target: fmt.Println("Counter", counter)
+	counter++ 
+	if (counter < 5){
+		goto target
+	}
+}
