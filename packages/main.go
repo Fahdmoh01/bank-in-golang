03babcd76c0ef3e77461fc91b0e8d99248b17050
Fahd Mohammed <fahdmoh.1@gmail.com>
2024-03-19 12:24:22 +0000

    done with creating and using packages

---
+++ packages/main.go
@@ -0,0 +1,29 @@
+package main
+
+// type Product struct{
+// 	Name, Category string
+// 	price float64
+// }
+
+import (
+	"fmt"
+	_ "packages/data"
+	. "packages/fmt"
+	"packages/store"
+	"packages/store/cart"
+	//"github.com/fatih/color"
+)
+
+func main() {
+	product := store.NewProduct("Kayak", "Watersports", 279)
+
+	cart := cart.Cart{
+		CustomerName: "Alice",
+		Products:     []store.Product{*product},
+	}
+
+	// color.Green("Name:", cart.CustomerName)
+	// color.Cyan("Total:", ToCurrency(cart.GetTotal()))
+	fmt.Println("Name:", cart.CustomerName)
+	fmt.Println("Total:", ToCurrency(cart.GetTotal()))
+}
