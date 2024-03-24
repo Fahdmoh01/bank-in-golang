7bfd833ad45dafc3e004bd4fb6d7e5c7da7a4f09
Fahd Mohammed <fahdmoh.1@gmail.com>
2024-03-24 11:53:02 +0000

    error handling completed

--- errorHandling/main.go
+++ errorHandling/main.go
@@ -1,16 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main(){
-	categories := []string{"Watersports", "Chess","Running"}
+	defer func(){
+		if arg := recover(); arg != nil {
+			if err, ok := arg.(error); ok{
+				fmt.Println("Error:",err.Error())
+				panic(err)
+			}else if str, ok := arg.(string); ok{
+				fmt.Println("Message:", str)
+			}else{
+				fmt.Println("Panic recovered")
+			}
+		}
+	}()
 
-	for _,cat := range categories{
-		total, err := Products.TotalPrice(cat)
-		if (err == nil){
-			fmt.Println(cat, "Total:", ToCurrency(total))
+	categories := []string{"Watersports", "Chess", "Running"}
+	channel := make(chan ChannelMessage, 10)
+	go Products.TotalPriceAsync(categories, channel)
+	for message := range channel{
+		if message.CategoryError == nil {
+			fmt.Println(message.Category, "Total:", ToCurrency(message.Total))
 		}else{
-			fmt.Println(cat,"(no such category)")
+			panic(message.CategoryError)
 		}
 	}
 }
